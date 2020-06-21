508294a7f320bbd9e1f3ae6b9e36040ea5a209c0
Julian Dunskus <julian.dunskus@gmail.com>
2020-06-21 05:17:37 +0200

    move verifier builder & context to prusti-common
    the server needs those, of course

---
+++ prusti-common/src/verification_service.rs
@@ -0,0 +1,33 @@
+use config;
+use viper::{self, VerificationBackend};
+use vir::Program;
+
+pub trait VerificationService {
+    fn verify(&self, request: VerificationRequest) -> viper::VerificationResult;
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct VerificationRequest {
+    pub program: Program,
+    pub program_name: String,
+    pub backend_config: ViperBackendConfig,
+}
+
+/**
+The configuration for the viper backend, (i.e. verifier).
+Expresses which backend (silicon or carbon) should be used, and provides command-line arguments to the viper verifier.
+*/
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Hash)]
+pub struct ViperBackendConfig {
+    pub backend: VerificationBackend,
+    pub verifier_args: Vec<String>,
+}
+
+impl Default for ViperBackendConfig {
+    fn default() -> Self {
+        Self {
+            backend: VerificationBackend::from_str(&config::viper_backend()),
+            verifier_args: config::extra_verifier_args(),
+        }
+    }
+}
