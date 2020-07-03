6f4c08d5cff47355d7b9682aa2db24256f3ab376
Vytautas Astrauskas <vastrauskas@gmail.com>
2020-07-03 16:00:50 +0200

    Move files from new to root.

---
+++ prusti-contracts/src/lib.rs
@@ -0,0 +1,33 @@
+extern crate proc_macro;
+
+#[cfg(not(feature = "prusti"))]
+mod private {
+    use proc_macro_hack::proc_macro_hack;
+
+    /// A macro for writing a precondition on a function.
+    pub use prusti_contracts_impl::requires;
+
+    /// A macro for writing a postcondition on a function.
+    pub use prusti_contracts_impl::ensures;
+
+    /// A macro for writing a loop invariant.
+    #[proc_macro_hack]
+    pub use prusti_contracts_impl::invariant;
+}
+
+#[cfg(feature = "prusti")]
+mod private {
+    use proc_macro_hack::proc_macro_hack;
+
+    /// A macro for writing a precondition on a function.
+    pub use prusti_contracts_internal::requires;
+
+    /// A macro for writing a postcondition on a function.
+    pub use prusti_contracts_internal::ensures;
+
+    /// A macro for writing a loop invariant.
+    #[proc_macro_hack]
+    pub use prusti_contracts_internal::invariant;
+}
+
+pub use private::*;
